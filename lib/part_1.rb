e34630f947c552023580b673d295880ded03cd58
Zane Vandergriff <zanevande@gmail.com>
2021-04-21 00:45:48 -0400

    Working on RSpec Exercises

--- lib/part_1.rb
+++ lib/part_1.rb
@@ -1,5 +1,32 @@
 def average(num_1, num_2)
 
     (num_1 + num_2) / 2.0
- #finishing exercise
+
+end
+
+def average_array(arr)
+
+   
+
+end
+
+def repeat(str, num)
+
+    str * num
+
+end
+
+def yell(str)
+
+    str.upcase + "!"
+
+end
+
+def alternating_case(str)
+    new_str = str.split(" ")
+
+
+
+
+    return 
 end
