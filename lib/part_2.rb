0d49f428becdc4010dcdcd8fdd2555cab64e2bf4
Zane Vandergriff <zanevande@gmail.com>
2021-04-24 13:26:49 -0400

    just adding some spaces for clarity

--- lib/part_2.rb
+++ lib/part_2.rb
@@ -1,16 +1,22 @@
 def hipsterfy(word)
 
-    word.reverse.sub(/[aeiou]/, "").reverse #learned .sub from ruby-doc.org, make this problem really easy.
+    word.reverse.sub(/[aeiou]/, "").reverse 
+    
+    #learned .sub from ruby-doc.org, make this problem really easy.
 
 end
+
 ######################################
+
 def vowel_counts(str)
     
     new_str = str.downcase
     new_str.gsub(/[^aeiou]/, "").each_char.each_with_object(Hash.new(0)) { |vowel, hash| hash[vowel] += 1 }
     
 end
+
 ######################################
+
 def caesar_cipher(str, num)
     letters = ('a'..'z').to_a
 
