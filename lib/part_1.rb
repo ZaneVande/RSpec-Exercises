ddb43bf9c625f31945b2a5266d3096ed4b67e262
Zane Vandergriff <zanevande@gmail.com>
2021-04-25 21:45:16 -0400

    Working on Part 2 Exercises

--- lib/part_1.rb
+++ lib/part_1.rb
@@ -1,23 +1,40 @@
 def partition(arr, num)
+    left = []
+    right = []
 
-
-    
+    arr.each do |ele|
+        if ele < num
+            left << ele
+        else
+            right << ele
+        end
+    end
+    [left, right]
 end
 
 #########################################
 
 def merge(hash_1, hash_2)
+    new_hash = {}
 
+    hash_1.each { |k, v| new_hash[k] = v }
+    hash_2.each { |k, v| new_hash[k] = v }
 
+    new_hash
 
 end
 
 #########################################
 
 def censor(sentence, arr)
-
-
-
+    words = sentence.split(" ")
+    words.map do |word|
+        if arr.include?(word.downcase)
+            word.gsub(/[aeiou]/i, "*")
+        else
+            word
+        end
+    end.join(" ")
 end
 
 #########################################
