3106c9ede6e1c27255ffd423b02b47ef31e796d2
Zane Vandergriff <zanevande@gmail.com>
2021-04-26 15:17:09 -0400

    Finished Part 2 of Part 2 RSpec Exercises

--- lib/part_2.rb
+++ lib/part_2.rb
@@ -1,15 +1,32 @@
 def palindrome?(str)
 
+    if str.length == 1 || str.length == 0
+        true
+    else
+        if str[0] == str[-1]
+            palindrome?(str[1..-2])
+        else
+            false
+        end
+    end
 end
 
 ##################################
 
 def substrings(str)
-
+    subs = []
+    (0...str.length).each do |start_idx|
+        (start_idx...str.length).each do |end_idx|
+            subs << str[start_idx..end_idx]
+        end
+    end
+    subs
 end
 
 ##################################
 
 def palindrome_substrings(str)
+
+    substrings(str).select { |sub_str| palindrome?(sub_str) && sub_str.length > 1 }
 
 end
